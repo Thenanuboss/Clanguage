f471c0cfee24f46d63d4b0d089d2ad946658ad18
Thenanuboss <163496069+Thenanuboss@users.noreply.github.com>
2024-03-15 10:17:25 +0530

    Variable file

---
+++ VARIABLE.C
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+
+//variable declaration//
+int a=12,b=6,c;
+
+void main(){
+	clrscr();
+	printf("variable declation");
+
+	printf("\n a = %d",a);
+	printf("\n b = %d",b);
+
+	printf("\n 12/6 = %d",a/b);
+	printf("\n addition %d",a+b);
+	printf("\n 12+6 = %d",a+b);
+	printf("\n subtraction %d",a-b);
+	printf("\n 12-6 = %d",a-b);
+	printf("\n multiplication %d",a*b);
+	printf("\n 12*6 = %d",a*b);
+	printf("\n Division %d",a/b);
+	printf("\n 12/6 = %d",a/b);
+	printf("\n modulo %d",a%b);
+	printf("\n 12%6 = 0",a%b);
+
+
+
+
+
+       getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
